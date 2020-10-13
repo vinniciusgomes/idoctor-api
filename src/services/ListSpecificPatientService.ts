95be9824e323f533c77eefbde6f90bc2416e6f8a
FelipeNS <novaisfelipe97@gmail.com>
2020-10-12 21:57:49 -0300

    Buscando paciente especifico pelo id

---
+++ src/services/ListSpecificPatientService.ts
@@ -0,0 +1,26 @@
+import { Equal, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import Patient from '../models/Patient';
+
+interface Request {
+  id: string;
+}
+
+class ListSpecificPatientService {
+  public async execute({ id }: Request): Promise<Patient | undefined> {
+    const patientRepository = getRepository(Patient);
+
+    try {
+      const patient = await patientRepository.findOne(id);
+
+      if (patient) {
+        return patient;
+      }
+    } catch {
+      throw new AppError('Erro on get patients list', 500);
+    }
+  }
+}
+
+export default ListSpecificPatientService;
