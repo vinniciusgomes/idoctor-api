01ca0bc651060a74cd506208fe5c057e72dc8642
FelipeNS <novaisfelipe97@gmail.com>
2020-11-18 17:15:59 -0300

    Configs para heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,6 @@
 module.exports  = {
   "type": "postgres",
-  "host": "ec2-54-196-89-124.compute-1.amazonaws.com",
-  "port": "5432",
-  "username": "nourlhtmyushww",
-  "password": "d54b32d01cdf576d2f32018a8a11611a5b15872144ca0230a813f829adcfdab3",
-  "database": "d5pe0u0qtg80d3",
+  "url": "postgres://nourlhtmyushww:d54b32d01cdf576d2f32018a8a11611a5b15872144ca0230a813f829adcfdab3@ec2-54-196-89-124.compute-1.amazonaws.com:5432/d5pe0u0qtg80d3",
   "migrations": ["./dist/database/migrations/*.js"],
   "cli": {
     "migrationsDir": "./dist/database/migrations"
