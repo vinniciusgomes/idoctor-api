b79fd05368ff39b67b2e9196f3615359d08f92a9
FelipeNS <novaisfelipe97@gmail.com>
2020-11-18 17:56:33 -0300

    Corrigindo listagem de pacientes

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,9 @@
 module.exports  = {
   "type": "postgres",
   "url": "postgres://nourlhtmyushww:d54b32d01cdf576d2f32018a8a11611a5b15872144ca0230a813f829adcfdab3@ec2-54-196-89-124.compute-1.amazonaws.com:5432/d5pe0u0qtg80d3",
+  "ssl": {
+    "rejectUnauthorized": false,
+  },
   "migrations": ["./dist/database/migrations/*.js"],
   "cli": {
     "migrationsDir": "./dist/database/migrations"
