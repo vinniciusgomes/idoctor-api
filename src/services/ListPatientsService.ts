fe78660de65a32549ac120aa1ce4906d4ddd61cc
FelipeNS <novaisfelipe97@gmail.com>
2020-11-18 17:58:55 -0300

    Corrigindo listagem de pacientes

--- src/services/ListPatientsService.ts
+++ src/services/ListPatientsService.ts
@@ -15,15 +15,15 @@ class ListPatientsService {
       const patientList = await patientRepository
         .createQueryBuilder('patient')
         .select(['patient.id', 'patient.name', 'patient.avatar'])
-        // .leftJoinAndSelect('patient.appointments', 'appointment')
-        // .where('appointment.status = :status', { status: 4 })
-        // .orWhere('appointment.type = :type', { type: 2 })
-        // .andWhere('appointment.status NOT IN (:canceled, :finished)', {
-        //   canceled: 3,
-        //   finished: 4,
-        // })
-        // .skip((Number(page) - 1) * 10)
-        // .take(10)
+        .leftJoinAndSelect('patient.appointments', 'appointment')
+        .where('appointment.status = :status', { status: 4 })
+        .orWhere('appointment.type = :type', { type: 2 })
+        .andWhere('appointment.status NOT IN (:canceled, :finished)', {
+          canceled: 3,
+          finished: 4,
+        })
+        .skip((Number(page) - 1) * 10)
+        .take(10)
         .orderBy('patient.name', 'ASC')
         .getMany();
 
